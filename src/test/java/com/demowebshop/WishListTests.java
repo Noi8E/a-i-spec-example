c93336c4405eed6f504b0a2feae658d6987a79d7
Noi8E <ftretyakov@kosmostyt.ru>
2022-01-17 16:18:08 +0300

    add wishlisttests

---
+++ src/test/java/com/demowebshop/WishListTests.java
@@ -0,0 +1,59 @@
+package com.demowebshop;
+
+
+
+import io.restassured.http.ContentType;
+import lombok.WishListResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.demowebshop.Specs.request;
+import static com.demowebshop.Specs.responseSpec;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static java.lang.Boolean.FALSE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class WishListTests extends TestBase {
+
+
+    @Test
+    @DisplayName("Can add item to Wishlist")
+    void canAddItemToWishList() {
+
+        step("try add Virtual Gift Card wo names", () -> {
+
+            WishListResponse wishListResponse =
+
+                given()
+                        .spec(request)
+                        .when()
+                        .post("/addproducttocart/details/2/2")
+                        .then()
+                        .spec(responseSpec)
+                        .log().body()
+                        .extract().as(WishListResponse.class);
+            assertThat(wishListResponse.getSuccess(), is(FALSE));
+            assertThat(wishListResponse.getMessage().get(1), is("Enter valid recipient email"));
+            assertThat(wishListResponse.getMessage().get(0), is("Enter valid recipient name"));
+    });
+
+        step("try add with required fields", () ->
+                given()
+                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                        .body("giftcard_2.RecipientName=Test&giftcard_2.RecipientEmail=test@test.ru" +
+                                "&giftcard_2.SenderName=Test2&giftcard_2.SenderEmail=test2@test.ru&" +
+                                "giftcard_2.Message=TestMessage&addtocart_2.EnteredQuantity=1")
+                        .log().all()
+                        .when()
+                        .post("http://demowebshop.tricentis.com/addproducttocart/details/2/2")
+                        .then()
+                        .spec(responseSpec)
+                        .log().body()
+                        .body("success", is(true))
+                        .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
+                );
+
+    }
+}
---
+++ src/test/java/lombok/WishListResponse.java
@@ -0,0 +1,14 @@
+package lombok;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class WishListResponse {
+    private Boolean success;
+    @Singular private List<String> message;
+
+}
